ae2f67771ea7a7a686535d8d564d23f16d257d00
Jérémy DEMANGE <j@lachainemedias.com>
2020-04-17 03:55:58 +0200

    Modification du message en header

--- src/components/MessageHaut.vue
+++ src/components/MessageHaut.vue
@@ -12,7 +12,7 @@ export default {
     return {
       alert: true,
       message:
-        "Mesdames, Messieurs. Nous vous rappelons que nous subissons actuellement un confinement strictes afin de réduire la contamination liée au virus COVID-19. Toute sortie n'est pas recommandé, sauf en cas de déplacement nécessaire et immédiat. Cette application n'est pas une application officielle. Nous avons repris une partie du code de l'application officielle. Le PDF générée sur cette application est entièrement valable en cas de contrôle. Notez aussi que nous ne stockons aucune donnée vous concernant (tout est stockée et reste sur votre appareil).",
+        "Mesdames, Messieurs. Nous vous rappelons que nous subissons actuellement un confinement strictes afin de réduire la contamination liée au virus COVID-19. Toute sortie n'est pas recommandé, sauf en cas de déplacement nécessaire et immédiat. Cette application n'est pas une application officielle. Nous avons repris une partie du code de l'application officielle. Le PDF générée sur cette application est entièrement valable en cas de contrôle. Notez aussi que nous ne stockons aucune donnée vous concernant (tout est stocké et reste sur votre appareil).",
     };
   },
 };
